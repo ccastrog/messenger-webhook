6d3c101c104925daf475190179237172306d4baa
ccastrog <castro.critian@outlook.com>
2018-10-16 15:44:57 -0500

    Get event type

--- index.js
+++ index.js
@@ -26,10 +26,19 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
     
+    
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
     
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
     });
 
     // Returns a '200 OK' response to all requests
@@ -83,4 +92,3 @@ function handlePostback(sender_psid, received_postback) {
 function callSendAPI(sender_psid, response) {
   
 }
-
