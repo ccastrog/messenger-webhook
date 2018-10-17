2b6d141ecaf8e7a8d6aa638ba2753cb468f676d8
ccastrog <castro.critian@outlook.com>
2018-10-17 09:01:03 -0500

    Change

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `Usted envió el mensaje: "${received_message.text}". Ahora envíe un adjunto!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
